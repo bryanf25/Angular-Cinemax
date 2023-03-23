c4c9e98f75c42a81c2166cede98d0703c0e11e2b
Brayan Camilo Flechas Torralba <bflechas@itcol.com>
2023-03-23 17:59:05 -0500

    inicio navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { NavbarSocialComponent } from './pages/components/navbar-social/navbar-social.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    NavbarSocialComponent
   ],
   imports: [
     BrowserModule
