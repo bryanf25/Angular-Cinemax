d0e2b802a8d78ddc5fc54e7bd8f42bf398d08106
Brayan Camilo Flechas Torralba <bflechas@itcol.com>
2023-03-23 20:15:18 -0500

    navbar 100%

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './pages/landing-page/landing-page.component';
 import { NavbarSocialComponent } from './pages/components/navbar-social/navbar-social.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +14,8 @@ import { NavbarSocialComponent } from './pages/components/navbar-social/navbar-s
     NavbarSocialComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/components/navbar-social/navbar-social.component.ts
+++ src/app/pages/components/navbar-social/navbar-social.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faFacebook, faInstagram, faTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-navbar-social',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar-social.component.css']
 })
 export class NavbarSocialComponent {
+  faFacebook = faFacebook;
+  faTwitter = faTwitter;
+  faYoutube = faYoutube;
+  faInstagram = faInstagram;
 
 }
