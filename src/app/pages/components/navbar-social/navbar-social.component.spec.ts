4663d6f27e6606e54b03eee80444b10c40842777
Brayan Camilo Flechas Torralba <bflechas@itcol.com>
2023-03-27 16:12:40 -0500

    correccion de unitTests

--- src/app/pages/components/navbar-social/navbar-social.component.spec.ts
+++ src/app/pages/components/navbar-social/navbar-social.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { NavbarSocialComponent } from './navbar-social.component';
 
@@ -8,7 +9,10 @@ describe('NavbarSocialComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ NavbarSocialComponent ]
+      declarations: [ NavbarSocialComponent ],
+      imports:[
+        FontAwesomeModule
+      ]
     })
     .compileComponents();
 
